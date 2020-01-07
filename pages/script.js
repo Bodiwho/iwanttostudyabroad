841ced8dfb75fa144703d3a1c73700762dbebc83
Bodiwho <44253549+Bodiwho@users.noreply.github.com>
2020-01-07 01:56:54 +0100

    update pages

---
+++ pages/script.js
@@ -0,0 +1,8 @@
+
+var re1= ["Teacher","Host Mom","Host Dad","Coordinator"];
+
+function answer1(){
+	var i=0;
+	for(i=0;i<4;i++){
+	document.getElementById("uno").innerHTML =  (re1[Math_Random(3)]);
+}}
