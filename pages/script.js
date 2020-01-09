cc0edc897fd2564c3139c97bbb58d1ac3433ef0d
Bodiwho <44253549+Bodiwho@users.noreply.github.com>
2020-01-09 01:35:37 +0100

    update pages

--- pages/script.js
+++ pages/script.js
@@ -1,8 +1,27 @@
 
 var re1= ["Teacher","Host Mom","Host Dad","Coordinator"];
 
-function answer1(){
-	var i=0;
-	for(i=0;i<4;i++){
-	document.getElementById("uno").innerHTML =  (re1[Math_Random(3)]);
-}}
+
+  var clicks = 0;
+  function answer1() {
+    document.getElementById("uno").innerHTML = clicks;
+}
+// function answer1(){
+// 	var num = 0;
+// 	var a = num+=1;
+// 	var text = re1[a];
+// 	// document.getElementById("uno").innerHTML =  (re1[Math_Random(3)])
+// 	console.log(text);
+// }
+		
+	
+	
+
+
+
+
+// function answer1(){
+// 	var i=0;
+// 	for(i=0;i<4;i++){
+// 	document.getElementById("uno").innerHTML =  (re1[Math_Random(3)]);
+// }}
